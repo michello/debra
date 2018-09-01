94c29065a28acc0cdfe29a54c7d12a863cb85584
ml4963 <ml4963@nyu.edu>
2018-09-01 16:51:51 -0400

    pushing text flow

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-# import Cookie
+import Cookie
+
 from twilio import twiml
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
+
 from flask import Flask, request, redirect, session, make_response
 
 app = Flask(__name__)
@@ -13,6 +15,8 @@ TWILIO_TOKEN = os.environ['TWILIO_TOKEN']
 
 t_client = Client(TWILIO_SID, TWILIO_TOKEN)
 
+C = Cookie.SimpleCookie()
+
 @app.route("/incoming_sms", methods=['GET', 'POST'])
 def incoming_sms():
   resp = MessagingResponse()
@@ -22,11 +26,17 @@ def incoming_sms():
     message_body = request.values.get('Body', None)
     
     if message_body == 'HI DEBRA':
-    	resp_message = "Hello! I am DEBRA (Disaster Emergency Bot Relief Alert). If you wish to receive supplies or help, please begin your message as 'I need these supplies...'. If you wish to provide supplies, please begin your message as 'I can provide...'"
-    elif 'I need these supplies' in message_body.upper():
-    	# save into db
+    	resp_message = "Hello! I am DEBRA (Disaster Emergency Bot Relief Alert). First, please provide the address you currently are at by typing, 'I am currently at...'"
+    elif 'I am currently at' in message_body:
+    	C["cookie_location"] = message_body.replace('I am currently at ','')
+    	resp_message = "Thanks! If you wish to receive supplies or help, please start your text either with 'I can provide...' or 'I need these supplies...'"
+    elif 'I need these supplies' in message_body:
+    	# save stuff into db
+    	C["cookie_request"] = message_body.replace('I am currently at ','')
     	resp_message = "Thanks for your request! I'll update you when there's someone with your supplies ASAP. (:"
-    elif 'I can provide...' in message_body:
+    elif 'I can provide' in message_body:
+    	# save stuff into db
+    	C["cookie_request"] = message_body.replace('I am currently at ','')
     	resp_message = "Thanks for your request! I'll update you."
     else:
     	resp_message = "Whoops, that is an invalid text! Feel free to text 'Help!' for a full list of commands. :)"
