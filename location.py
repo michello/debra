28b7beffd46685f73ee5e2c8f15ec7a8f16d3b76
ml4963 <ml4963@nyu.edu>
2018-09-01 22:30:00 -0400

    adding map

--- location.py
+++ location.py
@@ -1,12 +1,17 @@
-
-
+from geopy.geocoders import Nominatim
 from flask import Flask, request, redirect, session, make_response, render_template
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 @app.route("/location")
 def locations():
+	geolocator = Nominatim()
 	loc = ['917 63rd St, Brooklyn, NY']
-	return render_template("index.html", jslocation = loc)
+	coordinates = []
+	for locate in loc:
+		location = geolocator.geocode(locate)
+		coordinates.append([location.latitude, location.longitude])
+
+	return render_template("index.html", jslocation = coordinates)
 
 if __name__ == "__main__":
 	app.run('localhost', 5000)
