49e3a869cd4e7e3e80ed88f5d7efa92569511962
cindylee0113 <cindylee0113@gmail.com>
2018-09-02 03:02:38 -0400

    Adding number

--- location.py
+++ location.py
@@ -18,26 +18,33 @@ def locations():
 	for giver in givers:
 		giver_data = giver.to_dict()
 		loc_data = dict()
-		loc_data['address'] = giver_data[ u'location']
+		loc_data['number'] = giver_data['number']
+		loc_data['address'] = giver_data['location']
 		loc_data['item'] = giver_data[u'item']
 		loc_data['type'] = 'give'
+		try:
+			location = geolocator.geocode(loc_data['address'])
+			loc_data['coordinates'] = [location.latitude, location.longitude]
+		except:
+			loc_data['coordinates'] = [0.0, 0.0]
 		loc.append(loc_data)
 	for needer in needers:
 		needer_data = needer.to_dict()
 		loc_data = dict()
-		loc_data['address'] = needer_data[ u'location']
+		loc_data['number'] = needer_data['number']
+		loc_data['address'] = needer_data['location']
 		loc_data['item'] = needer_data[u'item']
 		loc_data['type'] = 'need'
-		loc.append(loc_data)
-	coordinates = []
-	for locate in loc:
 		try:
-			location = geolocator.geocode(locate)
+			location = geolocator.geocode(loc_data['address'])
 			coordinates.append([location.latitude, location.longitude])
+			loc_data['coordinates'] = [location.latitude, location.longitude]
 		except:
-			continue
+			loc_data['coordinates'] = [0.0, 0.0]
+		loc.append(loc_data)
+	print (loc)
 
-	return render_template("index.html", jslocation = coordinates)
+	return render_template("index.html", jslocation = loc)
 
 if __name__ == "__main__":
 	app.run('localhost', 5000)
